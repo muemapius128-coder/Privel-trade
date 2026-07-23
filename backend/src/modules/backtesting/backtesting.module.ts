f72000f1289176b1a627b3593ff4c37b281440ca
muemapius128-coder <muemapius128@gmail.com>
2026-07-23 17:03:06 +0300

    Update backtesting.module.ts

--- backend/src/modules/backtesting/backtesting.module.ts
+++ backend/src/modules/backtesting/backtesting.module.ts
@@ -1,17 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BacktestingController } from './controllers/backtesting.controller';
 import { BacktestingService } from './services/backtesting.service';
 import { BacktestingRepository } from './repositories/backtesting.repository';
 
+import { Backtest } from './entities/backtest.entity';
+import { BacktestRun } from './entities/backtest-run.entity';
+import { Strategy } from './entities/strategy.entity';
+import { TradeResult } from './entities/trade-result.entity';
+import { EquityCurve } from './entities/equity-curve.entity';
+import { PerformanceMetrics } from './entities/performance-metrics.entity';
+
 @Module({
-  controllers: [BacktestingController],
+  imports: [
+    TypeOrmModule.forFeature([
+      Backtest,
+      BacktestRun,
+      Strategy,
+      TradeResult,
+      EquityCurve,
+      PerformanceMetrics,
+    ]),
+  ],
+
+  controllers: [
+    BacktestingController,
+  ],
+
   providers: [
     BacktestingService,
     BacktestingRepository,
   ],
+
   exports: [
     BacktestingService,
+    BacktestingRepository,
   ],
 })
 export class BacktestingModule {}
